db66ff8d9a2baeb3d801424cf413c994103bddd6
Murshudlumaryam <maryamurshudlu@gmail.com>
2026-05-13 18:15:24 +0400

    Include sqlite dialect for Vercel

--- db/db.js
+++ db/db.js
@@ -1,8 +1,10 @@
 const { Sequelize } = require('sequelize');
 const path = require('path');
+const sqlite3 = require('sqlite3');
 
 const sequelize = new Sequelize({
   dialect: 'sqlite',
+  dialectModule: sqlite3,
   storage: process.env.VERCEL
     ? path.join('/tmp', 'database.sqlite')
     : path.join(__dirname, 'database.sqlite'),
